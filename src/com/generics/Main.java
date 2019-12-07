7ec8c5675ebbd00720203d3912d1b490a3e736fe
Asus-Tomek <szypki69047@gmail.com>
2019-12-07 12:31:46 +0100

    Pierwszy Upload

---
+++ src/com/generics/Main.java
@@ -0,0 +1,17 @@
+package com.generics;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int a,b,c;
+	    a = 6;
+        b = 3;
+        c = a+b;
+
+        System.out.println("6 + 3 = " + c);
+
+
+    }
+}
